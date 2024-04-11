e41ad334f1aae67c772ae70fb4a6879d46070e5a
Brody <brodyover@gmail.com>
2024-04-11 00:17:25 -0400

    revert back to `multirun`

--- start.sh
+++ start.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-pg_isready -h $PGHOST -p $PGPORT
+echo Waiting for database...
+
+while ! pg_isready -h ${PGHOST} -p ${PGPORT}; do sleep 0.25; done; 
+
+echo Database is now available
 
 bundle exec rails db:chatwoot_prepare
 
 bundle exec rails db:migrate
 
-exec parallel --ungroup --halt now,done=1 ::: \
+exec multirun \
     "bundle exec sidekiq -C config/sidekiq.yml" \
     "bundle exec rails s -b 0.0.0.0 -p $PORT"
