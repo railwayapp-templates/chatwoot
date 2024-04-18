fb10a0ad6eaa39129ee745c35ddac23aa797d693
Brody Over <10548119+brody192@users.noreply.github.com>
2024-04-17 21:35:19 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,5 +14,6 @@ bundle exec rails db:migrate
 
 multirun \
     "bundle exec sidekiq -C config/sidekiq.yml" \
-    "bundle exec rails s -b 0.0.0.0 -p $PORT"; \
-    false
+    "bundle exec rails s -b 0.0.0.0 -p $PORT"
+
+false
